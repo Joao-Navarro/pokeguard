6268a0bc898e92e37c6394b78ba8f763bd2e4d4b
Joao-Navarro <joao.navarro2@icloud.com>
2023-12-12 11:27:29 -0300

    tipos (problema no map)

---
+++ app/tipos/_app.js
@@ -0,0 +1,14 @@
+
+import '../styles/globals.css'
+
+
+function MyApp({ Component, pageProps, router }) {
+  return (
+
+    <Component {...pageProps} key={router.route} />
+
+  )
+
+}
+
+export default MyApp
